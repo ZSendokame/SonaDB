a45745f5e2fe1dd01d5610ca4f80330ec8dea44b
Sendo kame <sendo@sendokame.com>
2022-11-10 16:41:54 -0300

    Metadata.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 long_description = open('./README.md')
 
 setup(
-    name='kdb',
-    version='1.0.0',
-    url='https://github.com/ZSendokame/KDB',
+    name='SonaDB',
+    version='1.1.0',
+    url='https://github.com/ZSendokame/SonaDB',
     license='MIT license',
     author='ZSendokame',
     description='Key-Value database library, small and easy to use.',
     long_description=long_description.read(),
     long_description_content_type='text/markdown',
 
-    packages=(find_packages(include=['kdb']))
+    packages=(find_packages(include=['sona']))
 )
