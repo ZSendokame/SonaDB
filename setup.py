cebe5defee897e6be980fe485c0969c795a2d378
Sendo kame <sendo@sendokame.com>
2022-11-21 12:53:58 -0300

    Metadata.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('./README.md')
 
 setup(
     name='SonaDB',
-    version='1.1.0',
+    version='1.2.0',
     url='https://github.com/ZSendokame/SonaDB',
     license='MIT license',
     author='ZSendokame',
